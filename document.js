6af62707762a28b10c782a9401a7ff16262e63fd
bienguez123 <152401096+bienguez123@users.noreply.github.com>
2024-04-09 19:20:23 +0200

    Add files via upload

---
+++ document.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var footerElement = document.querySelector('.vfrc-footer--watermark'); // Adjust the selector as needed
+    if (footerElement) {
+        footerElement.textContent = 'gay ahh nigga';
+    }
+});
